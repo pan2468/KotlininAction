88531f006f8fb0a59539eeab2073f39314bb880f
임채성 <pan1129@naver.com>
2022-05-18 21:33:25 +0900

    Commit

---
+++ src/com/kotlin/generics/HigherOrderFunctions.kt
@@ -0,0 +1,7 @@
+package com.kotlin.generics
+
+fun main() {
+    val authors = listOf("Dmitry","Svetlana")
+    val readers = mutableListOf<String>(/* ... */)
+
+}
---
+++ src/com/kotlin/generics/ParameterConstraints.kt
@@ -0,0 +1,14 @@
+package com.kotlin.generics
+
+fun <T> ensureTrailingPeriod(seq: T)
+    where T: CharSequence, T : Appendable{
+        if(!seq.endsWith('.')){
+            seq.append('.')
+        }
+    }
+
+fun main() {
+    val helloWorld = StringBuilder("Hello World")
+    ensureTrailingPeriod(helloWorld)
+    println(helloWorld)
+}
