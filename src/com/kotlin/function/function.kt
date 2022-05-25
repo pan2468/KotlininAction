e85db84db5ad5ca19ac4ab75f8cc9ff239467171
임채성 <pan1129@naver.com>
2022-05-25 11:30:37 +0900

    Commit

--- src/com/kotlin/function/function.kt
+++ src/com/kotlin/function/function.kt
@@ -1,8 +1,10 @@
 package com.kotlin.function
 
-fun max(a: Int, b:Int): Int{
-    return if(a > b) a else b
-}
+//fun max(a: Int, b:Int): Int{
+//    return if(a > b) a else b
+//}
+
+fun max(a: Int,b:Int): Int = if(a > b) a else b
 
 fun main() {
     println(max(1,2))
