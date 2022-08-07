c54675b3b05c531a9ccb36f050766dd25c3ec819
임채성 <pan1129@naver.com>
2022-08-07 19:37:04 +0900

    문자열 템플릿 Commit

--- src/com/kotlin/chapters1/String.kt
+++ src/com/kotlin/chapters1/String.kt
@@ -1,6 +1,16 @@
 package com.kotlin.chapters1
 
-fun main(args: Array<String>){
-    val name = if(args.size > 0) args[0] else "Kotlin"
-    println("Hello, $name")
+//fun main(args: Array<String>){
+//    val name = if(args.size > 0) args[0] else "Kotlin"
+//    println("Hello, $name")
+//}
+
+//fun main(args: Array<String>) {
+//    if(args.size > 0){
+//        println("Hello, ${args[0]}!")
+//    }
+//}
+
+fun main(args: Array<String>) {
+    println("Hello, ${if (args.size > 0) args[0] else "someone"}!")
 }
