add6a2d7d029362b4752782cbbfae2e68de1b858
임채성 <pan1129@naver.com>
2023-01-05 00:03:17 +0900

    컬렉션 any 연산 API

--- src/com/kotlin/chapters5/CollectionOperations.kt
+++ src/com/kotlin/chapters5/CollectionOperations.kt
@@ -13,7 +13,8 @@ fun main(){
 
     val canBeInClub27 = {p: Person -> p.age <=27}
     val people = listOf(Person("Alice",27), Person("Bob",31))
-    println(people.all(canBeInClub27))
+    println(people.any(canBeInClub27))
+    // println(people.all(canBeInClub27))
     // println(people.count (canBeInClub27))
     //println(people.find(canBeInClub27))
 }
